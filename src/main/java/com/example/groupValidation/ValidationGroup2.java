2396c95e56a08c5d0f2fd08edf122f751ae0a28b
KunAce <wxkace@gmail.com>
2020-06-06 23:35:15 -0700

    Added regular validation and validation by group on the data entries from user

---
+++ src/main/java/com/example/groupValidation/ValidationGroup2.java
@@ -0,0 +1,4 @@
+package com.example.groupValidation;
+
+public interface ValidationGroup2 {
+}
---
+++ src/main/java/com/example/groupValidation/groupValidationApplication.java
@@ -0,0 +1,11 @@
+package com.example.groupValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class groupValidationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(groupValidationApplication.class, args);
+    }
+}
